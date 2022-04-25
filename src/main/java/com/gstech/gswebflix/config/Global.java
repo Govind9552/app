be67da201bd7c5660cb77afe8d1e19c9a8aee501
Govind Patil <govindshindepatil9552@gmail.com>
2022-04-25 22:54:10 +0530

    initial

---
+++ src/main/java/com/gstech/gswebflix/config/Global.java
@@ -0,0 +1,24 @@
+package com.virlabs.demo_flx_application.config;
+
+/**
+ * Created by Tamim on 28/09/2017.
+ */
+
+
+
+public class Global {
+    public static final String API_URL = "https://web.gswebflix.xyz/api/";
+    public static final String SECURE_KEY = "4F5A9C3D9A86FA54EACEDDD635185";
+
+    public static final String Youtube_Key = "AIzaSyAPy0-p_A2Iws1rPfySQ3fm-7K5q8oNyxw"; // get it from this link  https://console.developers.google.com/apis/credentials
+
+    public static final String ITEM_PURCHASE_CODE = "d506abfd-9fe2-4b71-b979-feff21bcad13";
+
+
+
+    public static final String SUBSCRIPTION_ID = "com.virlabs.demo_flx_application.subs";
+
+}
+
+
+
